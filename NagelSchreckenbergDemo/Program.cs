48c3f3bedbc9d163a06155c3d923c515a489bb53
dubielel <mdubielel@gmail.com>
2023-11-15 18:31:27 +0100

    First draft of NaSch CA prototype for traffic

---
+++ NagelSchreckenbergDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NagelSchreckenbergDemo;
+
+Console.WriteLine("Hello, World!");
+
+
+var cells = new int[100];
+for (int i = 0; i < cells.Length; i++)
+{
+    cells[i] = 0;
+}
+
+foreach (var cell in cells)
+{
+    Console.Write(cell);
+}
+Console.WriteLine("cells initialized");
+
+var v1 = new Vehicle(1, 5, ref cells);
+
+foreach (var cell in cells)
+{
+    Console.Write(cell);
+}
+Console.WriteLine("cells after first car");
+
+for (int i = 0; i < cells.Length; i++)
+{
+    cells[i] = 0;
+}
+foreach (var cell in cells)
+{
+    Console.Write(cell);
+}
+Console.WriteLine("cells cleared");
+
+var v2 = new Vehicle(1, 13, ref cells);
+foreach (var cell in cells)
+{
+    Console.Write(cell);
+}
+Console.WriteLine("cells after second car");
+
+//if ((new ArraySegment<int>(cells, 0, 13)).All(x => x == 0))
+//{
+//    var v2 = new Vehicle(1, 13, ref cells);
+//}
+
