cb91fc1e62ccd8485cc70953d9a66c99397fa3f0
dubielel <mdubielel@gmail.com>
2023-11-16 22:00:01 +0100

    Some cleanup and TrafficSimulator

--- NagelSchreckenbergDemo/Program.cs
+++ NagelSchreckenbergDemo/Program.cs
@@ -2,54 +2,6 @@
 using NagelSchreckenbergDemo;
 using NagelSchreckenbergDemo.DirectedGraph;
 
-void StateOut(DirectedGraph graph)
-{
-    foreach (var e in graph.edges)
-    {
-        Console.WriteLine("edge: " + e.id + " from " + e.startV.id + " to " + e.endV.id);
-        Console.Write(string.Join("", e.cells));
-        Console.WriteLine();
-    }
-}
-
-void MakeMoves(DirectedGraph graph, int n)
-{
-    StateOut(graph);
-
-    for (int i = 0; i < n; i++)
-    {
-        foreach (Edge e in graph.edges)
-            e.MakeMoves();
-
-        StateOut(graph);
-    }
-}
-
-var graph = new DirectedGraph();
-graph.AddVertex();
-graph.AddVertex();
-graph.AddVertex();
-
-graph.AddEdge(100, 0, 1);
-graph.AddEdge(100, 1, 2);
-
-foreach (var v in graph.vertices)
-{
-    Console.WriteLine(v.id);
-    foreach (var adj in v.adjacencyList) { Console.WriteLine("ADJ: " + adj.id); }
-}
-
-var v1 = new Vehicle(1, 5, graph.edges[0]);
-// var v2 = new Vehicle(1, 13, graph.edges[1]);
-
-// foreach (var e in graph.edges)
-// {
-//     Console.WriteLine("edge: " + e.id + " from " + e.startV.id + " to " + e.endV.id);
-//     foreach (var c in e.cells)
-//     {
-//         Console.Write(c);
-//     }
-//     Console.WriteLine();
-// }
-
-MakeMoves(graph, 24);
+TrafficSimulation simulator  = new TrafficSimulation();
+simulator.Iterate(30);
+//simulator.RealIterate();
--- NagelSchreckenbergDemo/TrafficSimulation.cs
+++ NagelSchreckenbergDemo/TrafficSimulation.cs
@@ -1,10 +1,83 @@
-using NagelSchreckenbergDemo.DirectedGraph;
+using NagelSchreckenbergDemo;
 using System;
 
 namespace NagelSchreckenbergDemo
 {
     public class TrafficSimulation
     {
-        
+        private DirectedGraph.DirectedGraph roadSystem;
+        private List<Vehicle> vehicles;
+
+        public TrafficSimulation()
+        {
+            roadSystem = new DirectedGraph.DirectedGraph();
+            vehicles = new List<Vehicle>();
+
+            this.Initialize();
+        }
+
+        private void Initialize()
+        {
+            roadSystem.AddVertex();
+            roadSystem.AddVertex();
+            roadSystem.AddVertex();
+
+            roadSystem.AddEdge(100, 0, 1);
+            roadSystem.AddEdge(100, 1, 2);
+
+            vehicles.Add(new Vehicle(1, 5, roadSystem.edges[0]));
+        }
+
+        private void Update()
+        {
+            foreach (var edge in roadSystem.edges)
+            {
+                edge.MakeMoves();
+            }
+        }
+
+        public void RealIterate()
+        {
+            int time = 0;
+            while (true)
+            {
+                time++;
+                if (time >= 61)
+                    time = 1;
+
+                foreach (var vehicle in vehicles)
+                {
+                    if (((vehicle.velocity * time) % 60) == 0)
+                    {
+                        vehicle.SingleStep();
+                        if (vehicle.toDelete)
+                            vehicle.edge.RemoveVehicle(vehicle);
+                        this.PrintState();
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Iterate(int nIterations)
+        {
+            for (int i = 0; i < nIterations; i++)
+            {
+                Console.WriteLine("Iteration: " + (i + 1));
+                this.Update();
+                this.PrintState();
+                Console.WriteLine("");
+                // Thread.Sleep(1000);
+            }
+        }
+
+        public void PrintState()
+        {
+            foreach (var edge in roadSystem.edges)
+            {
+                Console.WriteLine("Edge: " + edge.id + " starts from Vertex: " + edge.startV.id + " ends with Vertex: " + edge.endV.id);
+                Console.WriteLine(string.Join("", edge.cells));
+            }
+        }
     }
 }
